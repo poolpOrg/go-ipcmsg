04f996f41b07220a4576bd952bc45e5c16db607d
Gilles Chehade <gilles@poolp.org>
2023-01-14 09:17:03 +0100

    another example for a program that passes fd back and forth forever

---
+++ examples/04-fd-passing-forever/parent.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021 Gilles Chehade <gilles@poolp.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/poolpOrg/go-ipcmsg"
+)
+
+func parent() {
+	pid, fd := fork_child()
+	channel := ipcmsg.NewChannel("parent<->child", pid, fd)
+	channel.Handler(IPCMSG_PONG, handlePONG)
+
+	fp, err := os.Open("/etc/passwd")
+	if err != nil {
+		log.Fatal("could not open")
+	}
+	channel.Message(IPCMSG_PING, "PING ?", int(fp.Fd()))
+	<-channel.Dispatch()
+}
+
+func handlePONG(msg ipcmsg.IPCMessage) {
+	var data string
+	msg.Unmarshal(&data)
+
+	fmt.Printf("parent: got PONG with fd=%d from child: %s\n", msg.Fd(), data)
+	msg.Reply(IPCMSG_PING, "PING !", msg.Fd())
+}
---
+++ examples/04-fd-passing-forever/plumbing.go
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2021 Gilles Chehade <gilles@poolp.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/poolpOrg/go-ipcmsg"
+)
+
+var (
+	IPCMSG_PING ipcmsg.IPCMsgType = ipcmsg.NewIPCMsgType("")
+	IPCMSG_PONG ipcmsg.IPCMsgType = ipcmsg.NewIPCMsgType("")
+)
+
+// upon execution, call parent() which will setup a socketpair
+// then fork a child to reexec the program with the REEXEC env
+// var set to CHILD, making it execute child().
+func main() {
+	reexec := os.Getenv("REEXEC")
+	switch reexec {
+	case "":
+		parent()
+	case "CHILD":
+		child()
+	}
+}
+
+// fork_child() sets up the socketpair to be shared by parent and child,
+// passing one end as fd 3 to child & returning the other end to parent.
+// the child reexecutes the program with env var REEXEC.
+func fork_child() (int, int) {
+	binary, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sp, err := syscall.Socketpair(syscall.AF_LOCAL, syscall.SOCK_STREAM, syscall.AF_UNSPEC)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// XXX - not quite there yet
+	//syscall.SetNonblock(sp[0], true)
+	//syscall.SetNonblock(sp[1], true)
+
+	procAttr := syscall.ProcAttr{}
+	procAttr.Files = []uintptr{
+		uintptr(syscall.Stdin),
+		uintptr(syscall.Stdout),
+		uintptr(syscall.Stderr),
+		uintptr(sp[0]),
+	}
+	procAttr.Env = []string{
+		"REEXEC=CHILD",
+	}
+
+	var pid int
+
+	pid, err = syscall.ForkExec(binary, []string{os.Args[0]}, &procAttr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if syscall.Close(sp[0]) != nil {
+		log.Fatal(err)
+	}
+
+	return pid, sp[1]
+}
